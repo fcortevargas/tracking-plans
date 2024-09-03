bbf23b8eeb46c5470b403636cf0ad3cbe4403eb6
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2024-09-03 09:00:09 +0200

    🔧 fix: Fixes to config file.

--- app/config.py
+++ app/config.py
@@ -1,12 +1,11 @@
-import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    rudderstack_api_key: str = os.getenv("RUDDERSTACK_API_KEY")
-    notion_api_key: str = os.getenv("NOTION_API_KEY")
-    catalog_id: str = os.getenv("RUDDERSTACK_CATALOG_ID")
-    parent_page_id: str = os.getenv("NOTION_PARENT_PAGE_ID")
+    rudderstack_api_token: str
+    notion_api_token: str
+    rudderstack_base_url: str
+    notion_parent_page_id: str
 
     class Config:
         env_file = ".env"
